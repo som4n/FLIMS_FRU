60c9004077ad8e43248306f364d7f90a8ddd7548
som4n <61178177+som4n@users.noreply.github.com>
2023-06-30 23:02:25 +0530

    Dependencies
    Added eslint and packages

---
+++ flims/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import {Route,Switch } from 'react-router-dom';
+
+const App =() =>(
+        <div>
+        <CssBaseline/>
+           <h1>
+           App
+           </h1> 
+        </div>
+    );
+
+export default App;
