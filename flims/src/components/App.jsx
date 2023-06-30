e48747926703126bc7e04f012b8877d4afbd15fc
som4n <61178177+som4n@users.noreply.github.com>
2023-07-01 02:56:20 +0530

    routing
    Added Routes

--- flims/src/components/App.jsx
+++ flims/src/components/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import {Route,Switch } from 'react-router-dom';
+import { Switch , Route } from "react-router-dom";
 
 const App =() =>(
-        <div>
+    <div>
         <CssBaseline/>
-           <h1>
-           App
-           </h1> 
-        </div>
-    );
+        <main>
+            <Switch>
+                <Route exact path="/movie/:id">
+                    <h1>Movie Information</h1>
+                </Route>
+                <Route exact path="/actors/:id">
+                    <h1>Actors Information</h1>
+                </Route>                
+                <Route exact path="/profile/:id">
+                    <h1>Profile</h1>
+                </Route>
+                <Route exact path="/">
+                    <h1>Movies</h1>
+                </Route>
+            </Switch> 
+        </main>
+    </div>
+);
 
 export default App;
